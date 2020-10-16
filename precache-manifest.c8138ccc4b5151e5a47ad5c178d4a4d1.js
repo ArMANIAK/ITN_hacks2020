cbed52e7bc1dda38a75beffad3b14a10b8a607d6
ArMANIAK <reklama.alatyr@gmail.com>
2020-10-16 23:24:22 +0300

    Updates

---
+++ precache-manifest.c8138ccc4b5151e5a47ad5c178d4a4d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c49098388cc3076fb4dc797c65f8c33e",
+    "url": "/ITN_hacks2020/index.html"
+  },
+  {
+    "revision": "a166b57a0fcec9f11b7e",
+    "url": "/ITN_hacks2020/static/css/main.5968ef5a.chunk.css"
+  },
+  {
+    "revision": "7da64edbba2cdb6a9ac8",
+    "url": "/ITN_hacks2020/static/js/2.fee08d3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ITN_hacks2020/static/js/2.fee08d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a166b57a0fcec9f11b7e",
+    "url": "/ITN_hacks2020/static/js/main.cef5f4c6.chunk.js"
+  },
+  {
+    "revision": "92c5ef30af8498fa03df",
+    "url": "/ITN_hacks2020/static/js/runtime-main.1d954994.js"
+  },
+  {
+    "revision": "304e5212f4da364e03f792b6d5fdd200",
+    "url": "/ITN_hacks2020/static/media/BazaIT.304e5212.svg"
+  }
+]);
