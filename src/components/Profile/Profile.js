ad27ebeb8c74b8f4a4ac5804cd56db357cd008c6
ArMANIAK <reklama.alatyr@gmail.com>
2020-10-17 20:00:43 +0300

    applied markup and styling

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -56,27 +56,69 @@ const Profile = () => {
             ['2020-08', 17000],
             ['2020-09', 19000],
         ]};
+
+    const activeUsers = {
+        'Active Users': [
+            ['2020-09-15', 25],
+            ['2020-09-18', 17],
+            ['2020-09-19', 23],
+            ['2020-09-21', 20],
+            ['2020-09-22', 8],
+            ['2020-09-23', 22],
+            ['2020-09-25', 24],
+            ['2020-10-15', 15],
+            ['2020-10-25', 27],
+        ]
+    }
   
     return (
         <div className="profile">
-            <nav className="profile-tab">
-                <ul>
-                    <li><a href="#" title="link to tab">Profile Statistics</a></li>
-                    <li className="active-tab"><a href="#" title="link to tab">General Statistics</a></li>
-                    <li><a href="#" title="link to tab">Comparative Statistics</a></li>
-                    <li><a href="#" title="link to tab">Company Statistics</a></li>
-                </ul>
-            </nav>
-            <div className="profile-tab">
-                <nav className="profile-tab-nav">
+            <section className="section">
+                <h2 className="section-title">Аналітика</h2>
+                <nav class="section-nav">
                     <ul>
-                        <li className="active"><button onClick = {() => setStats(vacancies)}>Vacancies per Day</button></li>
-                        <li className="active"><button onClick = {() => setStats(geography)}>Vacancies Geography</button></li>
-                        <li className="active"><button onClick = {() => setStats(averageSalary)}>Average Salary</button></li>
+                        <li class="section-nav-text">Платформа</li>
+                        <li class="section-nav-text">Профіль</li>
+                        <li class="section-nav-text">Компания</li>
                     </ul>
+                    
                 </nav>
-                <Statistics stats={stats}/>
-            </div>
+                <main class="container">
+                    <nav class="aside">
+                        <ul class="aside-ul">
+                            <li class="aside-li">
+                                <button class="aside-li-text" onClick = {() => setStats(vacancies)}>Нових вакансій</button>
+                            </li>
+                            <li class="aside-li">
+                                <button class="aside-li-text" onClick = {() => setStats(geography)}>Географія вакансій</button>
+                            </li>
+                            <li class="aside-li">
+                                <button class="aside-li-text" onClick = {() => setStats(averageSalary)}>Средня заробітна плата</button>
+                            </li>
+                            <li class="aside-li">
+                                <button class="aside-li-text" onClick = {() => setStats(activeUsers)}>Активних користувачів</button>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <main class="section">
+                        <nav class="section-nav">
+                            <ul class="nav-profile">
+                                <li class="nav-profile-item">День</li>
+                                <li class="nav-profile-item">Тиждень</li>
+                                <li class="nav-profile-item">Місяць</li>
+                                <li class="nav-profile-item">Рік</li>
+                            </ul>
+                            <input type="text" name="text" id="text" placeholder="Введіть міста через кому" disabled/>
+                            <input type="date" name="date" id="date"/>
+                        </nav>
+                        <div class="graphic">
+                            <Statistics stats={stats}/>
+                        </div>
+                    </main>
+                </main>
+            </section>
+
             <Tester />
         </div>
     )
