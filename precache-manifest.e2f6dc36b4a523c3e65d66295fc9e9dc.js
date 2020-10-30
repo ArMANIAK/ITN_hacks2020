7107636561ac6fef633ee358ab02a2490bdbdd6d
vladimir3700 <vladdibov23@gmail.com>
2020-10-30 17:22:48 +0300

    Updates

---
+++ precache-manifest.e2f6dc36b4a523c3e65d66295fc9e9dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb37426b57eaf27c0501dfd005c895c",
+    "url": "/ITN_HACKATON_2020-second-place-/index.html"
+  },
+  {
+    "revision": "f6482f6594f8d9f846a0",
+    "url": "/ITN_HACKATON_2020-second-place-/static/css/main.bcbfd93e.chunk.css"
+  },
+  {
+    "revision": "ed420cd85b34feb98470",
+    "url": "/ITN_HACKATON_2020-second-place-/static/js/2.7c0f37c0.chunk.js"
+  },
+  {
+    "revision": "c83eaf460a8f55ef068d9679da1b7145",
+    "url": "/ITN_HACKATON_2020-second-place-/static/js/2.7c0f37c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6482f6594f8d9f846a0",
+    "url": "/ITN_HACKATON_2020-second-place-/static/js/main.fcdec8a0.chunk.js"
+  },
+  {
+    "revision": "286309dc236b84b8a0e9",
+    "url": "/ITN_HACKATON_2020-second-place-/static/js/runtime-main.bff3879f.js"
+  },
+  {
+    "revision": "304e5212f4da364e03f792b6d5fdd200",
+    "url": "/ITN_HACKATON_2020-second-place-/static/media/BazaIT.304e5212.svg"
+  }
+]);
